bad92eacda043ec87f90678d85ec2425e9894ac1
Lewis Reid <lreidwilliams@gmail.com>
2023-05-07 00:22:46 +1000

    Implemented Text Completion (#2)
    * implemented naive text completion
    
    * added counter to suggestion record for use in purging inactive suggestions
    
    * added condition to only notice terms with 3 or more characters to avoid suggesting meaningless phrases
    
    * added side effect in notice to purge a random old suggestion to maintain popular results
    
    * added guard clause to skip hitting redis when checking suggests for a blank string'

---
+++ lib/text_completion.rb
@@ -0,0 +1,70 @@
+class TextCompletion
+
+  def initialize(store, key:, purge_threshold: 10)
+    @store = store
+    @key = key
+    @purge_threshold = purge_threshold.to_i
+  end
+
+  def suggest(text, limit: 10)
+    return [] unless terms.match?(/\w+/)
+
+    @store.zrange(@key, "[#{text}", "[#{text}\xff", limit: [0, limit], bylex: true)
+      .map { |suggestion| suggestion[/.*:/m].chop! }
+      .compact
+  end
+
+  def notice(terms)
+    tap do
+      if terms.match?(/\w{3,}/)
+        purge_old_suggestion
+        processed_terms = terms.delete(':')
+        processed_terms.strip!
+        @store.eval(notice_term_script, [@key], [processed_terms])
+      end
+    end
+  end
+
+  def inspect
+    "#{self.class}:#{@key}"
+  end
+
+private
+
+  def purge_old_suggestion
+    count, record = @store.multi do |r|
+      r.zcard(@key)
+      r.zrandmember(@key)
+    end
+
+    if count >= @purge_threshold
+      suggestion, hits = *record.split(':')
+      hits = hits.to_i
+
+      @store.multi do |r|
+        @store.zrem(@key, record)
+        @store.zadd(@key, 0, "#{suggestion}:#{hits.pred}") if hits > 1
+      end
+    end
+  end
+
+  def notice_term_script
+    @notice_term_script ||= %Q(
+      local term = ARGV[1]
+      local index_key = KEYS[1]
+      local record = redis.call("zrange", index_key, "[" .. term .. ":", "[" .. term .. ":\xff", "BYLEX", "LIMIT", 0, 1)[1]
+      local next_record = nil
+
+      if record then
+        redis.call("zrem", index_key, record)
+        local counter = tonumber(string.match(record, ":(%d+)")) or 0
+        next_record = term .. ":" .. counter + 1
+      else
+        next_record = term .. ":1"
+      end
+
+      redis.call("zadd", index_key, 0, next_record)
+    )
+  end
+
+end
---
+++ spec/examples/text_completion_spec.rb
@@ -0,0 +1,61 @@
+require 'text_completion'
+
+RSpec.describe TextCompletion do
+
+  describe '#suggest' do
+
+    let(:text) { 'dog' }
+    let(:completion) { described_class.new(redis, key: 'test_completion') }
+    subject { completion.suggest(text) }
+
+    it 'has no suggestions when no previous searches were noticed' do
+      is_expected.to match []
+    end
+
+    it 'does not provide suggestions when input text is blank' do
+      completion.notice('advance').notice('artist').notice('ark')
+      expect(completion.suggest('')).to match []
+    end
+
+    it 'returns similar terms that were previously noticed' do
+      completion.notice('cat').notice('mouse').notice('deer')
+      similar_terms = %w(doggy doge dogger dogod)
+      similar_terms.each { |t| completion.notice(t) }
+      is_expected.to match_array similar_terms
+    end
+
+    it 'returns the most similar term when limited' do
+      similar_terms = %w(doge doggy doggg)
+      similar_terms.each { |t| completion.notice(t) }
+      expect(completion.suggest(text, limit: 1)).to contain_exactly(similar_terms.first)
+    end
+
+    it 'removes the delimiter from terms if present' do
+      completion.notice(text + ':')
+      expect(completion.suggest(text)).to contain_exactly(text)
+    end
+
+    it 'does not suggest searches with less than 3 characters' do
+      completion.notice('a').notice(' a ').notice('a             a')
+      expect(completion.suggest('a')).to match []
+    end
+
+    it 'purges an older suggestion when noticing new terms' do
+      old_suggestions = %w(apple adam ample arbiter after artsy adder another assume augment)
+      old_suggestions.each { |t| completion.notice(t) }
+      completion.notice('bambi')
+      suggestions = completion.suggest('a')
+      expect(suggestions.size).to eq old_suggestions.size - 1
+      expect(suggestions.all? { |s| old_suggestions.include?(s) }).to be true
+    end
+
+    it 'does not purge older popular suggestions' do
+      2.times { completion.notice(text) }
+      always_purge_completion = described_class.new(redis, key: 'test_completion', purge_threshold: 1)
+      always_purge_completion.notice('cat')
+      expect(always_purge_completion.suggest(text)).to contain_exactly(text)
+    end
+
+  end
+
+end
