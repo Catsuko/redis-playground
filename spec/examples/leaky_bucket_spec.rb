1c4173214ceb97156a6e1acdd1c35b06a8f7a843
Lewis Reid <lreidwilliams@gmail.com>
2023-05-08 04:05:55 +1000

    added leaky logic to the leaky bucket via redis script

--- spec/examples/leaky_bucket_spec.rb
+++ spec/examples/leaky_bucket_spec.rb
@@ -1,48 +1,51 @@
 require 'leaky_bucket'
+require 'timecop'
 
 RSpec.describe LeakyBucket do
 
   let(:capacity) { 3 }
-  let(:bucket) { described_class.new(redis, key: 'test_bucket', capacity: capacity) }
-
-  describe '#full?' do
-    subject { bucket.full? }
-
-    it 'is not full when empty' do
-      is_expected.to eq false      
-    end
-  end
+  let(:leak_rate) { 10 }
+  let(:bucket) { described_class.new(redis, key: 'test_bucket', capacity: capacity, leak_rate: leak_rate) }
 
   describe '#fill' do
     subject { bucket.fill }
 
-    it 'returns the current size of the bucket' do
-      is_expected.to eq 1
+    it 'returns `nil` when the bucket was filled' do
+      is_expected.to be_nil
     end
 
-    it 'fills the bucket when reaching capacity' do
-      expect { capacity.times { bucket.fill } }.to change(bucket, :full?).from(false).to(true)
+    it 'returns the amount of seconds before the bucket can be filled when full' do
+      capacity.times { bucket.fill }
+      is_expected.to be_within(1).of(leak_rate)
+    end
+
+    it 'can be filled when at capacity if enough time passes' do
+      capacity.times { bucket.fill }
+      Timecop.freeze(Time.now + leak_rate + 1) do
+        is_expected.to be_nil
+      end
     end
   end
 
-  describe '#remaining' do
-    subject { bucket.remaining }
+  describe '#fill?' do
+    subject { bucket.fill? }
 
-    it 'returns the capacity when the bucket is empty' do
-      is_expected.to eq capacity
+    it 'returns true when the bucket was filled' do
+      is_expected.to eq true
     end
 
-    it 'returns the number of fills remaining when the bucket has been partially filled' do
-      expect { bucket.fill }.to change(bucket, :remaining).from(capacity).to(capacity - 1)
+    it 'returns false when the bucket could not be filled' do
+      capacity.times { bucket.fill }
+      is_expected.to eq false
     end
   end
 
   describe '#tip' do
     subject { bucket.tip }
 
-    it 'empties the bucket' do
+    it 'empties the bucket when full' do
       capacity.times { bucket.fill }
-      expect { subject }.to change(bucket, :remaining).from(0).to(capacity)
+      expect { subject }.to change(bucket, :fill?).from(false).to(true)
     end
   end
 
