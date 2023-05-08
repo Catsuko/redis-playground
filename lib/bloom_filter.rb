3acd2989f5a1ec4cb984fa13486014b26217dbde
Lewis Reid <lreidwilliams@gmail.com>
2023-05-09 02:05:55 +1000

    implemented bloom filter via redis bitmask

---
+++ lib/bloom_filter.rb
@@ -0,0 +1,29 @@
+class BloomFilter
+
+  def initialize(store, key:, capacity: 1000)
+    @store = store
+    @key = key
+    @capacity = capacity
+  end
+
+  def include?(item, &block)
+    @store.getbit(@key, offset_for(item)).positive? && (
+      !block_given? || !!block.call(item)
+    )
+  end
+
+  def add(item)
+    tap { @store.setbit(@key, offset_for(item), 1) }
+  end
+
+  def clear
+    tap { @store.del(@key) }
+  end
+
+private
+
+  def offset_for(item)
+    item.hash % @capacity
+  end
+
+end
